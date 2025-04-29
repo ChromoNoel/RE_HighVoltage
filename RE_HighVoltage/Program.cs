4a680c9cb54c1a065c945d9b9bdbe0e40cdacdb6
ChromoNoel <noelkarolyk@gmail.com>
2025-04-29 09:59:52 +0200

    kesz

--- RE_HighVoltage/Program.cs
+++ RE_HighVoltage/Program.cs
@@ -97,18 +97,29 @@ namespace RE_HighVoltage
                 }
             }
 
+            string LeltarBekeres()
+            {
+                string szam = Console.ReadLine();
+                if (szam == string.Empty || szam == "0" || szam == null || szam.Length != 9)
+                {
+                    return string.Empty;
+                }
+
+                if (szam[..3] != "LPT" || !szam[3..].All(x => Char.IsDigit(x)))
+                {
+                    return string.Empty;
+                }
+
+                return szam;
+            }
+
             void Feladat_6(List<Berles> berlesek)
             {
                 Console.WriteLine("6. feladat:");
                 Console.Write("\tKeresett leltári szám: ");
-                string? szam = Console.ReadLine();
+                string szam = LeltarBekeres();
 
-                if (szam == string.Empty || szam == "0" || szam == null || szam.Length != 9)
-                {
-                    return;
-                }
-
-                if (szam[..3] != "LPT" || !szam[3..].All(x => Char.IsDigit(x)))
+                if (szam == string.Empty)
                 {
                     return;
                 }
@@ -126,13 +137,27 @@ namespace RE_HighVoltage
 
             void Feladat_7(List<Berles> berlesek)
             {
+                var ossz = berlesek.Sum(x => (x.EndDate.DayNumber - x.StartDate.DayNumber + 1) * x.DailyFee);
+                var kauciobevetelek = berlesek.Sum(x => x.UseDeposit ? x.Deposit : 0);
 
+                Console.WriteLine($"7. feladat: A cég összes bevétele: {ossz + kauciobevetelek:# ### ###}");
             }
 
-            //Feladat_3(berles);
-            //Feladat_4(berles);
-            //Feladat_5(berles);
-            //Feladat_6(berles);
+            void Feladat_8(List<Berles> berlesek)
+            {
+                Console.Write("Keresett leltári szám: ");
+                string szam = LeltarBekeres();
+
+                var atlag = berlesek.Sum(x => x.Invnumber == szam ? x.Uptime : 0) / berlesek.Where(x => x.Invnumber == szam).Count();
+                Console.WriteLine($"8. feladat: Az {szam} leltári számú laptop bérlésenkénti átlagos üzemideje: {Math.Round(atlag, 2)}");
+            }
+
+            Feladat_3(berles);
+            Feladat_4(berles);
+            Feladat_5(berles);
+            Feladat_6(berles);
+            Feladat_7(berles);
+            Feladat_8(berles);
         }
     }
 }
